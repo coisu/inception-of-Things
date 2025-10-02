c4f5fa6ff33e9096448640a88adccec0e23a98a1
Jisu CHOI <jischoi@f2r11s8.clusters.42paris.fr>
2025-10-02 20:14:07 +0200

    bonus is workinggit add .

--- bonus/scripts/gitlab.sh
+++ bonus/scripts/gitlab.sh
@@ -28,14 +28,31 @@ helm upgrade --install gitlab gitlab/gitlab \
   --timeout 900s
 
 # waiting Pod to be ready
-kubectl wait --for=condition=ready --timeout=1200s pod -l app=webservice -n gitlab
+# kubectl wait --for=condition=ready --timeout=1200s pod -l app=webservice -n gitlab
 
 # GitLab pw
 echo -e "${GREEN}GitLab root password:"
 kubectl get secret gitlab-gitlab-initial-root-password -n gitlab -o jsonpath="{.data.password}" | base64 --decode
 echo -e "${RESET}"
 
-# port-forwarding 
-# kubectl port-forward svc/gitlab-webservice-default -n gitlab 8081:8080 >/dev/null 2>&1 &
-# kubectl port-forward svc/gitlab-webservice-default -n gitlab 8081:8080
-kubectl port-forward svc/gitlab-webservice-default -n gitlab 8081:8080 --address 0.0.0.0 & echo $! > /tmp/gitlab_pf.pid
+cat <<EOF
+check if every pods are running   kubectl get pods -n gitlab
+
+kubectl get svc -n gitlab
+
+when it's ready to port forwarding    kubectl port-forward svc/gitlab-webservice-default -n gitlab 8081:8181 --address 0.0.0.0
+Forwarding from 0.0.0.0:8081 -> 8181
+go to   http://localhost:8081 and create a new public project, check if the branch is protected
+
+git init
+git remote add origin http://localhost:8081/root/{project-name}.git
+push needed files...
+
+check deployment...
+  kubectl get pods -n dev
+  kubectl get svc -n dev
+  kubectl get ingress -n dev
+
+
+
+EOF
--- bonus/scripts/repo-update.sh
+++ bonus/scripts/repo-update.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
-set -e
+set -euo pipefail
 
 # --- Configuration ---
-ARGO_APP_FILE="../confs/deploy.yaml"
-NEW_REPO_URL="http://localhost:8081/root/buthor.git"
+APP_FILE="../confs/application.yaml"
+PROJ_FILE="../confs/project.yaml"
+NEW_REPO_URL="http://localhost:8081/root/bonus-app.git"
 
-echo "--- Updating Argo CD application manifest ---"
+echo "--- Updating Argo CD manifests ---"
 echo "Old repoURL:"
-grep "repoURL" "$ARGO_APP_FILE"
+grep "repoURL" "$APP_FILE"
 
-# replacing
-sed -i.bak "s|repoURL:.*|repoURL: $NEW_REPO_URL|" "$ARGO_APP_FILE"
+# Replace only in application.yaml
+sed -i.bak "s|repoURL:.*|repoURL: $NEW_REPO_URL|" "$APP_FILE"
 
 echo "New repoURL:"
-grep "repoURL" "$ARGO_APP_FILE"
+grep "repoURL" "$APP_FILE"
 
 echo "--- Applying updated configuration to the cluster ---"
-kubectl apply -f "$ARGO_APP_FILE"
+kubectl apply -f "$PROJ_FILE"
+kubectl apply -f "$APP_FILE"
 
 echo "Argo CD is now pointing to local GitLab repository!"
+echo
+echo "If GitLab port-forward is not running, start it with:"
+
